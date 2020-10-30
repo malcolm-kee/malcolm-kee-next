eef5585a1f7ebd3045c2b25a7f5263eeaecf51ec
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-10-30 13:24:15 +0800

    auto deploy to Netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <h1>Home</h1>
+      <p>Auto-deploy to Netlify!</p>
     </div>
   );
 }
